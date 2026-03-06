17e35d14fd50b328f9387535424908c986ae921b
vktrenga <vktrenga@gmail.com>
2026-03-06 18:09:16 +0530

    hostname

--- fastapi-mig-ci-cd/app/main.py
+++ fastapi-mig-ci-cd/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+import socket
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "FastAPI deployed with MIG + GitHub Actions"}
+    return {
+        "message": "FastAPI MIG LoadBalancer",
+        "host": socket.gethostname()
+    }
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}

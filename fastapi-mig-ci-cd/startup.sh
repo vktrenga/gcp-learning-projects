ff8c6c200966a29989284a72db38a18e97cc7b03
vktrenga <vktrenga@gmail.com>
2026-03-06 19:30:24 +0530

    git ignore

--- fastapi-mig-ci-cd/startup.sh
+++ fastapi-mig-ci-cd/startup.sh
@@ -5,7 +5,7 @@ apt install -y python3-pip git
 
 git clone https://github.com/vktrenga/gcp-learning-projects.git
 
-cd gcp-learning-projects.git/fastapi-mig-ci-cd
+cd gcp-learning-projects/fastapi-mig-ci-cd
 
 pip3 install -r requirements.txt
 

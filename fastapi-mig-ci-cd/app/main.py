052880d270948dd0788eca0b5cec9549f5f7d790
vktrenga <vktrenga@gmail.com>
2026-03-07 22:23:34 +0530

    ci&cd

--- fastapi-mig-ci-cd/app/main.py
+++ fastapi-mig-ci-cd/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {
-        "message": "FastAPI MIG LoadBalancer ",
+        "message": "FastAPI MIG LoadBalancer Edited",
         "host": socket.gethostname(),
         "port":  8000
     }

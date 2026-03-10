6a7981e08a9e47c9997626a1f1fa2f7210fe59c5
vktrenga <vktrenga@gmail.com>
2026-03-10 18:41:43 +0530

    docker added

---
+++ fastapi-mig-ci-cd-docker/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import socket
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {
+        "message": "FastAPI MIG LoadBalancer Edited",
+        "host": socket.gethostname(),
+        "port":  8000
+    }
+
+@app.get("/health")
+def health():
+    return {"status": "ok", "host": socket.gethostname(), "port": 8000}
